ed7e7f14059ccae5473cb5e0d597afe99837b83b
Mike Wong <mikewongtkd@gmail.com>
2025-12-23 21:54:36 -0800

    Adding routes

---
+++ frontend/psf/src/pages/NotFound.js
@@ -0,0 +1,8 @@
+function NotFound() {
+  return (
+    <div className="NotFound">
+    </div>
+  );
+}
+
+export default NotFound;
---
+++ frontend/psf/src/pages/Register.js
@@ -0,0 +1,13 @@
+import Container from 'react-bootstrap/Container';
+
+function Register() {
+  return (
+    <Container>
+        <div className="Register">
+            <h1>Choose a Role</h1>
+        </div>
+    </Container>
+  );
+}
+
+export default Register;
